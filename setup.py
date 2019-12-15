a7769a66ba2ec684e066007ab3f3acac3b385aa1
Blarc <jakob.malezic@gmail.com>
2019-12-15 20:23:18 +0100

    added setup.py for pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="sledenje-objektom-2",
+    version="0.0.1",
+    description="sledenje-objektom-2",
+    url="git@github.com:blarc/sledenje-objektom-2.git",
+    author="Jakob Maležič",
+    author_email="jakob.malezic@gmail.com",
+    license="unlicense",
+    packages=["sledenje-objektom-2"],
+    zip_safe=False
+)
