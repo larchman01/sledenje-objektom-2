dc4cb8fe52d21dca9a3643a69a589302e65c12c4
Blarc <jakob.malezic@gmail.com>
2020-02-08 12:43:33 +0100

    MovableObject now has Point attribute instead of x, y

--- sledilnik/classes/MovableObject.py
+++ sledilnik/classes/MovableObject.py
@@ -1,12 +1,14 @@
+from sledilnik.classes.Point import Point
+
+
 class MovableObject:
     def __init__(self, x, y, dir):
-        self.x = x
-        self.y = y
+        self.pos = Point(x, y)
         self.direction = dir
 
     def reprJSON(self):
         return {
-            "x": self.x,
-            "y": self.y,
+            "x": self.pos.x,
+            "y": self.pos.y,
             "dir": self.direction
         }
