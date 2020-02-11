170d4261f1b7a041537a2c9feca20a35ce073b47
Blarc <jakob.malezic@gmail.com>
2020-02-11 18:17:06 +0100

    fields fix

--- sledilnik/classes/Field.py
+++ sledilnik/classes/Field.py
@@ -2,7 +2,7 @@ from sledilnik.classes.Point import Point
 
 
 class Field:
-    def __init__(self, topRight: Point, bottomRight: Point, bottomLeft: Point, topLeft: Point):
+    def __init__(self, topLeft: Point, topRight: Point, bottomRight: Point, bottomLeft: Point):
         self.topLeft = topLeft
         self.topRight = topRight
         self.bottomLeft = bottomLeft
