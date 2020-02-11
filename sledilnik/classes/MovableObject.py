d5090bca5a1556185c3b59fe281d36500e5190c2
Blarc <jakob.malezic@gmail.com>
2020-02-11 14:34:17 +0100

    added attribute id to MovableObject.py

--- sledilnik/classes/MovableObject.py
+++ sledilnik/classes/MovableObject.py
@@ -2,12 +2,14 @@ from sledilnik.classes.Point import Point
 
 
 class MovableObject:
-    def __init__(self, x, y, dir):
+    def __init__(self, id: int, x, y, dir):
+        self.id = id
         self.pos = Point(x, y)
         self.direction = dir
 
     def reprJSON(self):
         return {
+            "id": self.id,
             "x": self.pos.x,
             "y": self.pos.y,
             "dir": self.direction
