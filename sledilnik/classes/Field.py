598b88f5e4f02d72714d5b09d3f638218ec10bd6
Blarc <jakob.malezic@gmail.com>
2023-03-08 13:51:08 +0100

    fix: Order of bottom_right and bottom_left in Field.

--- sledilnik/classes/Field.py
+++ sledilnik/classes/Field.py
@@ -4,26 +4,26 @@ from sledilnik.classes.Point import Point
 
 
 class Field:
-    def __init__(self, top_left: Point, top_right: Point, bottom_left: Point, bottom_right: Point):
+    def __init__(self, top_left: Point, top_right: Point, bottom_right: Point, bottom_left: Point):
         self.top_left: Point = top_left
         self.top_right: Point = top_right
-        self.bottom_left: Point = bottom_left
         self.bottom_right: Point = bottom_right
+        self.bottom_left: Point = bottom_left
 
     def to_json(self):
         return {
             "top_left": self.top_left.to_json(),
             "top_right": self.top_right.to_json(),
-            "bottom_left": self.bottom_left.to_json(),
-            "bottom_right": self.bottom_right.to_json()
+            "bottom_right": self.bottom_right.to_json(),
+            "bottom_left": self.bottom_left.to_json()
         }
 
     def to_tuple(self) -> tuple:
         return (
             self.top_left.to_tuple(),
             self.top_right.to_tuple(),
-            self.bottom_left.to_tuple(),
-            self.bottom_right.to_tuple()
+            self.bottom_right.to_tuple(),
+            self.bottom_left.to_tuple()
         )
 
     @classmethod
@@ -31,6 +31,6 @@ class Field:
         return api.model('Field', {
             'top_left': fields.Nested(Point.to_model(api), required=True),
             'top_right': fields.Nested(Point.to_model(api), required=True),
-            'bottom_left': fields.Nested(Point.to_model(api), required=True),
             'bottom_right': fields.Nested(Point.to_model(api), required=True),
+            'bottom_left': fields.Nested(Point.to_model(api), required=True),
         })
