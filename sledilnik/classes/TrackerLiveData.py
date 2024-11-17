d8e42e13ce6765ab8bcde909da5b7f764cc6cfd0
larchman01 <larkunc@gmail.com>
2024-11-17 14:05:08 +0100

    Testing timestamp

--- sledilnik/classes/TrackerLiveData.py
+++ sledilnik/classes/TrackerLiveData.py
@@ -8,9 +8,11 @@ class TrackerLiveData:
     def __init__(self, fields: Dict[int, Field]):
         self.fields: Dict[int, Field] = fields
         self.objects: Dict[int, ObjectTracker] = {}
+        self.timestamp = None
 
     def to_json(self):
         return {
             "fields": {str(field_id): field.to_json() for field_id, field in self.fields.items()},
-            "objects": {str(object_id): game_object.to_json() for object_id, game_object in self.objects.items()}
+            "objects": {str(object_id): game_object.to_json() for object_id, game_object in self.objects.items()},
+            "timestamp": self.timestamp
         }
